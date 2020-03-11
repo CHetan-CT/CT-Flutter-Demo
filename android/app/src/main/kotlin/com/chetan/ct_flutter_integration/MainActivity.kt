65aeffd0cde75d4f5017bda9b342c80493a01270
Chetan <chetan.ashtivkar@clevertap.com>
2020-03-11 18:54:26 +0530

    Basic flutter project with CT SDK

--- android/app/src/main/kotlin/com/chetan/ct_flutter_integration/MainActivity.kt
+++ android/app/src/main/kotlin/com/chetan/ct_flutter_integration/MainActivity.kt
@@ -4,9 +4,13 @@ import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin
+//import dev.flutter.plugins.e2e.E2EPlugin
 
 class MainActivity: FlutterActivity() {
     override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
         GeneratedPluginRegistrant.registerWith(flutterEngine);
+        flutterEngine.plugins.add(FirebaseMessagingPlugin())
+//        flutterEngine.plugins.add(E2EPlugin())
     }
 }
--- android/app/src/main/kotlin/com/chetan/ct_flutter_integration/MyApplication.java
+++ android/app/src/main/kotlin/com/chetan/ct_flutter_integration/MyApplication.java
@@ -1,14 +1,26 @@
 package com.chetan.ct_flutter_integration;
 
-//import com.clevertap.android.sdk.ActivityLifecycleCallback;
+
+import com.clevertap.android.sdk.ActivityLifecycleCallback;
+import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback;
 
 import io.flutter.app.FlutterApplication;
+import io.flutter.plugin.common.PluginRegistry;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+import io.flutter.plugins.firebasemessaging.FlutterFirebaseMessagingService;
+import io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin;
 
-public class MyApplication extends FlutterApplication {
+public class MyApplication extends FlutterApplication implements PluginRegistrantCallback {
 
     @java.lang.Override
     public void onCreate() {
-//        ActivityLifecycleCallback.register(this);
+        ActivityLifecycleCallback.register(this);
+        FlutterFirebaseMessagingService.setPluginRegistrant(this);
         super.onCreate();
+    }
+
+    @Override
+    public void registerWith(PluginRegistry pluginRegistry) {
+        FirebaseMessagingPlugin.registerWith(pluginRegistry.registrarFor("io.flutter.plugins.firebasemessaging.FirebaseMessagingPlugin"));
     }
 }
